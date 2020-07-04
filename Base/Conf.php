98a0f95f0e12d590a8b60b7c5cb3c8eb3459423b
陈水兴 <335193057@qq.com>
2020-07-04 11:15:55 +0800

    一个使用php制作的图书管理系统

---
+++ Base/Conf.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    //数据库配置
+    'db_host'   =>  "127.0.0.1",
+    'db_user'   =>  "123456",
+    'db_pwd'    =>  "123456",
+    'db_name'   =>  "mybook",
+    'db_port'   =>  "3305",
+    'charset'   =>  "utf8",
+
+    //默认路由配置
+    'default_plantform'   => "Common",
+    'default_controller'  => "Login",
+    'default_action'      => "index"
+);
